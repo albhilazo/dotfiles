c5d8da8f4384a119a24f367f27b9545124f7e55a
Albert Hilazo <albhilazo@gmail.com>
2015-11-01 00:44:38 +0100

    New aliases

---
+++ oh-my-zsh/aliases.zsh
@@ -0,0 +1,56 @@
+# Custom aliases
+
+
+# cd
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+
+
+# ls
+alias ls='ls -F'
+alias ll='ls -lAh'
+alias la='ls -laAh'
+alias lt='ls -ltr'
+
+
+# git
+alias g='git'
+alias gsta='git status'
+alias grem='git remote'
+alias gbra='git branch'
+alias gchk='git checkout'
+alias gadd='git add'
+alias gadda='git add --all'
+alias gcom='git commit'
+alias gcomm='git commit -m'
+alias gfet='git fetch'
+alias gpus='git push'
+alias gpul='git pull'
+
+
+# vagrant
+alias v='vagrant'
+alias vup='vagrant up'
+alias vsus='vagrant suspend'
+alias vhal='vagrant halt'
+alias vrel='vagrant reload'
+alias vssh='vagrant ssh'
+alias vv='vagrant up && vagrant ssh'
+
+
+# directories
+alias dbox='cd /data/Dropbox/'
+alias proj='cd /data/Dropbox/projects/'
+
+
+# other
+alias clr='clear'
+alias md='mkdir -p'
+alias rm='rmdir'
+alias betty='/opt/betty/main.rb'
+alias tres='trs {en=es}'
+
+
+# new
