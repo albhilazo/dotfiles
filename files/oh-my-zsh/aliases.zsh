d7b54fd2bcf2f64f530c611f42a32394fa5075f9
Albert Hilazo <albhilazo@gmail.com>
2018-03-09 12:33:26 +0100

    Add docker run aliases

--- files/oh-my-zsh/aliases.zsh
+++ files/oh-my-zsh/aliases.zsh
@@ -20,6 +20,8 @@ alias g='git'
 # docker
 alias d='docker'
 alias dc='docker-compose'
+alias d-run='docker run -ti --rm'
+alias d-run-v='docker run -ti --rm -w /code -v $(pwd):/code'
 alias d-rm-exited='docker rm $(docker ps -a -f status=exited -q)'
 alias d-rmi-dangling='docker rmi $(docker images -f dangling=true -q)'
 alias d-volume-rm-dangling='docker volume rm $(docker volume ls -f dangling=true -q)'
