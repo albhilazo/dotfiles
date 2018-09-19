9e554c93ee4501eff7e97a646315d2ad189f1d37
Albert Hilazo <albhilazo@gmail.com>
2018-09-19 10:55:11 +0200

    Add aws alias that uses a container for the cli

--- files/oh-my-zsh/aliases.zsh
+++ files/oh-my-zsh/aliases.zsh
@@ -32,5 +32,7 @@ alias d-volume-rm-dangling='docker volume rm $(docker volume ls -f dangling=true
 # other
 alias clr='clear'
 
+alias aws='docker run --rm -ti -e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" -e "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" -e "AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}" -v "$(pwd):/project" mesosphere/aws-cli'
+
 
 # new
